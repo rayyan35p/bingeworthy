8ec533f5173ef44eb95794f843893251ba42ea09
Rayyan <rayyanis0908@gmail.com>
2023-05-16 16:53:44 +0800

    can now see more info on each show in json format

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 class query_results:
     
-    def __init__(self, id, poster_url):
+    def __init__(self, id, media_type, poster_url):
         self.id = id
+        self.media_type = media_type
         self.poster_url = poster_url
     
 
@@ -20,6 +21,7 @@ def results():
     query = request.form['query']
     #print(query.replace(" ", "+"))
     payload = {'api_key' : '013394cc2a0b549c132a73bfc223372e', 'query' : query}
+    # the request only returns the first page of results... need to figure out a way to get the next pages
     r = requests.get('https://api.themoviedb.org/3/search/multi', params=payload)
     #print(r.json())
     for value in r.json().get('results'):
@@ -39,16 +41,27 @@ def results():
         try:
             poster_url = "https://image.tmdb.org/t/p/w500" + result.get('poster_path')
         except:
+            # placeholder url
             poster_url = "https://image.tmdb.org/t/p/w500/w3rXpniqssYcppC5UwuQfP1scVB.jpg"
         print(image_url)
-        query = query_results(result.get('id'), poster_url)
+        query = query_results(result.get('id'), result.get('media_type'), poster_url)
         query_result_list.append(query)
         
     return render_template('results.html', results=query_result_list)
     
-@app.route('/shows/<int:id>')
-def shows(id):
-    return render_template('shows.html')
+@app.route('/movie/<int:id>')
+def movie(id):
+    payload = {'api_key' : '013394cc2a0b549c132a73bfc223372e'}
+    r = requests.get('https://api.themoviedb.org/3/movie/' + str(id) , params=payload)
+    movie_details = r.json()
+    return render_template('movies.html', movie_details=movie_details)
+
+@app.route('/tv/<int:id>')
+def tv(id):
+    payload = {'api_key' : '013394cc2a0b549c132a73bfc223372e'}
+    r = requests.get('https://api.themoviedb.org/3/tv/' + str(id) , params=payload)
+    tv_details = r.json()
+    return render_template('tv.html', tv_details=tv_details)
 
 
 if __name__ == "__main__":
