f858301f1b3c82aa07006331338e6b898cb614fc
Rayyan <rayyanis0908@gmail.com>
2023-05-26 22:52:12 +0800

    added storing of user details to database

--- project/auth.py
+++ project/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, redirect, render_template,  url_for
+from flask import Blueprint, redirect, render_template, request, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -13,6 +15,19 @@ def signup():
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        return redirect('/signup')
+    
+    new_user = User(email=email, password=generate_password_hash(password, method='sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
     return redirect('/login')
 
 @auth.route('/logout')
--- project/models.py
+++ project/models.py
@@ -4,4 +4,3 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
