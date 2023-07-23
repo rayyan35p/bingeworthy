dc9db1f121fe1de7761e0c1e63dee3319a4c278b
Benson <bensonang36@gmail.com>
2023-07-24 00:12:22 +0800

    added test

--- tests/test_project.py
+++ tests/test_project.py
@@ -43,4 +43,27 @@ def test_addMovieToList(client, app):
     with app.app_context():
         assert show_list.query.count() == 2
         assert show_list.query.first().shows[0].show_id == "557"
+
+def test_follow(client, app):
+    with app.app_context():
+        u1 = User(name='john', email='john@example.com')
+        u2 = User(name='susan', email='susan@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        assert u1.followed.all() == []
+        assert u1.followers.all() == []
+        u1.follow(u2)
+        db.session.commit()
+        assert u1.is_following(u2)== True
+        assert u1.followed.count() == 1
+        assert u1.followed.first().name == 'susan'
+        assert u2.followers.count() == 1
+        assert u2.followers.first().name == 'john'
+        u1.unfollow(u2)
+        db.session.commit()
+        assert u1.is_following(u2)== False
+        assert u2.followers.count() == 0
+        assert u1.followed.count() == 0
+
     
